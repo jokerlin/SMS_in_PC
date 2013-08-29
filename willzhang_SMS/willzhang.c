75022f835d822d692de2e7a0b70c522c5d7f2975
willzhang4a58 <willzhang_bit@163.com>
2013-08-29 16:13:33 +0800

    some initial function

---
+++ willzhang_SMS/willzhang.c
@@ -0,0 +1,44 @@
+#ifndef c_willzhang_c
+#define c_willzhang_c
+
+#include"willzhang.h"
+
+#include<sys/mman.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<fcntl.h>
+#include<unistd.h>
+#include<stdio.h>
+
+void DataBaseStart()
+{
+    int fd=open("DataFile",O_RDWR);
+    struct stat DataFileState;
+    fstat(fd,&DataFileState);
+    MemPoint=mmap(NULL,DataFileState.st_size,PROT_READ|PROT_WRITE,MAP_SHARED,fd,0);
+    DataHead=(long long *)MemPoint;
+    PersonList=((struct person *)(MemPoint+sizeof(long long)));
+    close(fd);
+    MemPoint+=DataHead[0];
+}
+void DataInit()
+{
+    int fd=open("DataFile",O_RDWR);
+    struct stat DataFileState;
+    fstat(fd,&DataFileState);
+    void *TmpMemPoint=mmap(NULL,DataFileState.st_size,PROT_READ|PROT_WRITE,MAP_SHARED,fd,0);
+    close(fd);
+    ((long long *)TmpMemPoint)[0]=sizeof(long long)+sizeof(struct person);
+    ((struct person *)(TmpMemPoint+sizeof(long long)))->HeadMessage=NULL;
+    ((struct person *)(TmpMemPoint+sizeof(long long)))->id=11111111111;
+    ((struct person *)(TmpMemPoint+sizeof(long long)))->name[0]='w';
+    ((struct person *)(TmpMemPoint+sizeof(long long)))->name[1]='z';
+    ((struct person *)(TmpMemPoint+sizeof(long long)))->name[2]='\0';
+    ((struct person *)(TmpMemPoint+sizeof(long long)))->Time=0;
+}
+int main()
+{
+    return 0;
+}
+
+#endif
---
+++ willzhang_SMS/willzhang.h
@@ -0,0 +1,25 @@
+#ifndef c_willzhang_h
+#define c_willzhang_h
+#include<time.h>
+struct message
+{
+    unsigned char content[71];
+    struct message *NextMessage;
+};
+
+struct person
+{
+    long long id;//11位电话号码
+    char name[21];//昵称为最长20位的ASCII码
+    time_t Time;//该联系人的最新一条信息的时间
+    struct message *HeadMessage;//指向第一条信息
+};
+
+long long *DataHead;//文件内存的第一个long long大小的内存指针，存储的内容为未使用的文件内存首地址
+void *MemPoint;//未使用的文件内存首地址
+struct person *PersonList;//文件内存的继第一个long long之后的第一个person大小的内存的指针，指向第一个联系人（开发者）
+void DataBaseStart();//开机时调用，为DataHead和MemPoint和PersonList赋初值
+void DataInit();//第一次开机时调用，初始化文件内存
+//内存存的前两个变量,一个long long ,一个person
+
+#endif
