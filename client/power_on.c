445c2b6fd2f4fe783485b0b83ba73785ebe2588e
linheng <anonymous.joker.lin@gmail.com>
2013-09-03 14:47:13 +0800

    在主函数folk了子进程，开机时增加了读取配置文件功能

--- client/power_on.c
+++ client/power_on.c
@@ -5,9 +5,13 @@
     > Created Time: Fri 30 Aug 2013 09:50:55 PM CST
  ************************************************************************/
 #include "power_on.h"
+#include "getprofile.h"
 #include <stdio.h>
 
 long long local_phoneNumber;
+char[16] server_ip;
+int local_port;
+int server_port;
 
 void welcome()
 {
@@ -19,8 +23,25 @@ void welcome()
 
 void power_on()
 {
-	//printf("Please Enter Your Telephone Number: "); 
-	//scanf("%lld",&phoneNumber);//输入电话号码
-	//list_person(1);//
-	//sock_power_on();
+	int flag = 0;//读取配置文件成功标志，0为成功，-1为失败
+	flag = GetProfileString("./client.conf","server","ip",server_ip);
+	if (flag == -1)
+	{
+
+	}
+
+	flag = GetProfileString("./client.conf","server","port",server_port);
+	if (flag == -1)
+	{
+
+	}
+	flag = GetProfileString("./client.conf","client","port",local_port);
+	if (flag == -1)
+	{
+
+	}
+	printf("Please Enter Your Telephone Number: "); 
+	scanf("%lld",&local_phoneNumber);//输入电话号码
+	list_person(1);
+	sock_power_on(longlong_to_string(local_phoneNumber),server_ip,server_port,local_port);
 }
--- client/sms.c
+++ client/sms.c
@@ -5,35 +5,46 @@
     > Created Time: Fri 30 Aug 2013 09:26:23 PM CST
  ************************************************************************/
 
-#include <stdio.h>
 #include <string.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
 #include "power_on.h"
 #include "kbhit.h"
 #include "client.h"
 
 int normal_power_flag = 0;//正常关机标志
 char instruction;
+pid_t pid;
 
 int main(int argc, char** argv)
 {
 	welcome();
 	power_on();
-	while (!normal_power_flag)
+	if (pid > 0)
 	{
-		while (!kbhit()) nothing();
-		instruction = getchar();
-		switch (instruction)
+		while (!normal_power_flag)
 		{
-			case 'q': normal_power_flag = 1;power_off(); break;
-			case 'h': help(); break;
-			case 'l': client_list_person(); break;
-			case 'm': client_list_person_message(); break;
-			case 'r': client_delete_person(); break;
-			case 'd': client_delete_message(); break;
-			case 's': send_message(); break;
-			case 'f': client_search_message(); break;
-			case 10: printf("NO INSTRUCTION. Please press 'h' for help.\n");break;
-			default: printf("WRONG INSTRUCTION. Please press 'h' for help.\n");
+			while (!kbhit()) nothing();
+			instruction = getchar();
+			switch (instruction)
+			{
+				case 'q': normal_power_flag = 1;power_off(); break;
+				case 'h': help(); break;
+				case 'l': client_list_person(); break;
+				case 'm': client_list_person_message(); break;
+				case 'r': client_delete_person(); break;
+				case 'd': client_delete_message(); break;
+				case 's': send_message(); break;
+				case 'f': client_search_message(); break;
+				case 10: printf("NO INSTRUCTION. Please press 'h' for help.\n");break;
+				default: printf("WRONG INSTRUCTION. Please press 'h' for help.\n");
+			}
 		}
+	}
+	else
+	{
+		receive_message();
 	}
 }
