1160229d33ea7a6bad08827bdee4a52f2be681bc
linheng <anonymous.joker.lin@gmail.com>
2013-09-03 14:15:48 +0800

    read profile

---
+++ client/getprofile.h
@@ -0,0 +1,15 @@
+/*************************************************************************
+    > File Name: getprofile.h
+    > Author: linheng
+    > Mail: i@haroquee.me 
+    > Created Time: Tue 03 Sep 2013 02:12:08 PM CST
+ ************************************************************************/
+
+#ifndef c_personoperation_h
+#define c_personoperation_h
+
+int GetProfileString(char *profile, char *AppName, char *KeyName, char *KeyVal );
+
+#endif
+
+
